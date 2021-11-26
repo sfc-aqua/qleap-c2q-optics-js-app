90c74c1da71488ee76a2786179766c37e5c4326d
martian17 <yakumodanshi@gmail.com>
2021-11-27 08:56:30 +0900

    fix formatting

--- src/pages/FourierAnalysis/waveForm.js
+++ src/pages/FourierAnalysis/waveForm.js
@@ -1,48 +1,49 @@
 class WaveForm {
-    //public
-    harmonics;
-    points;
-    degree;
-    constructor({ harmonics, points, degree }) {
-      if (harmonics) {//construct points
-        this.harmonics = [...harmonics];
-        this.degree = harmonics.length;
-      } else if (points && degree) {//construct harmonics
-        this.degree = degree();
-        this.points = points;
-        this.fourierTransform();//generates harmonics
-      } else {
-        throw new Error("not enough information to construct a wave");
-      }
-      //if either points or harmonics are incomplete
-    }
-  
-    waveHeightAt(r) {
-      let result = 0;
-      for (let i = 0; i < this.degree; i++) {
-        result += Math.cos(r * i) * this.harmonics[i];
-      }
-      return result;
-    }
-  
-    getHeights(width) {
-      const result = [];
-      for (let i = 0; i < width; i++) {
-        result.push(this.waveHeightAt(i / width * 2 * Math.PI));
-      }
-      return result;
-    }
-  
-    fourierTransform() {
-      //does fourier transform based on this.points
-      //for now it's just a mockup
-      let harmonics = [];
-      for (let i = 0; i < this.degree; i++) {
-        harmonics.push(1);//mockup value
-      }
-      this.harmonics = harmonics;
-      return harmonics;
-    }
-  };
+  //public
+  harmonics;
+  points;
+  degree;
 
-  export default WaveForm;
+  constructor({ harmonics, points, degree }) {
+    if (harmonics) {//construct points
+      this.harmonics = [...harmonics];
+      this.degree = harmonics.length;
+    } else if (points && degree) {//construct harmonics
+      this.degree = degree();
+      this.points = points;
+      this.fourierTransform();//generates harmonics
+    } else {
+      throw new Error("not enough information to construct a wave");
+    }
+    //if either points or harmonics are incomplete
+  }
+
+  waveHeightAt(r) {
+    let result = 0;
+    for (let i = 0; i < this.degree; i++) {
+      const [phase,amplitude] = this.harmonics[i];
+      result += Math.cos(phase+r * i) * amplitude;
+    }
+    return result;
+  }
+
+  getHeights(width) {
+    const result = [];
+    for (let i = 0; i < width; i++) {
+      result.push(this.waveHeightAt(i / width * 2 * Math.PI));
+    }
+    return result;
+  }
+
+  fourierTransform() {
+    //does fourier transform based on this.points
+    //for now it's just a mockup
+    let harmonics = [];
+    for (let i = 0; i < this.degree; i++) {
+      harmonics.push(1);//mockup value
+    }
+    this.harmonics = harmonics;
+    return harmonics;
+  }
+};
+export default WaveForm;
