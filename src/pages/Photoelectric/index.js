8f8599b7b311220c6afc5e3a5506868d8e900f82
martian17 <yakumodanshi@gmail.com>
2021-11-26 21:24:38 +0900

    delete test code (waveCanvas) from photoelectric

--- src/pages/Photoelectric/index.js
+++ src/pages/Photoelectric/index.js
@@ -1,10 +1,8 @@
 import React from "react";
-import WaveCanvas from "../../WaveCanvas";
 
 const Photoelectric = () => {
   return <div>
     <h1>Photoelectric Effect</h1>
-    <WaveCanvas />
   </div>;
 };
 
