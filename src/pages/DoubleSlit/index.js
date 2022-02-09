ff9dc502b84cf7c3a3b96429787daa2516bd5191
Metric55 <swifty312000@hotmail.com>
2022-02-09 15:19:28 +0900

    remove unwanted code. Remove debugging code.

--- src/pages/DoubleSlit/index.js
+++ src/pages/DoubleSlit/index.js
@@ -1,63 +1,39 @@
-import React, {useState} from "react";
-
-function Hello(props) {
-  console.log(props);
-  return <h2>Hello {props.name} : {props.age}</h2>
-}
-
-function handleClick() {
-  console.log("clicked")
-}
-
-function Counter() {
-  const [count, setCount] = useState(0);
-  console.log("render", count)
-
-  return <div>
-    <div>count: {count}</div>
-    <button onClick={() => setCount(count + 1)}>increment</button>
-  </div>
-}
-
+import React, { useState } from "react";
 
 function DoubleSlit() {
   const [Lambda, setLambda] = useState(1);
 
-    function handleSliderChangeL(Lambda) {
-      setLambda(Lambda.target.value);
-      console.log(Lambda.target.value);
-    }
+  function handleSliderChangeL(e) {
+    setLambda(e.target.value);
+  }
   const [S, setS] = useState(2);
 
-    function handleSliderChangeS(S) {
-      setS(S.target.value);
-      console.log(S.target.value);
-    }
+  function handleSliderChangeS(e) {
+    setS(e.target.value);
+  }
   const [A, setA] = useState(3);
 
-    function handleSliderChangeA(A) {
-      setA(A.target.value);
-      console.log(A.target.value);
-    }
+  function handleSliderChangeA(e) {
+    setA(e.target.value);
+  }
 
-  
   return (
     <div>
       <h1>Double-Slit Experiment (or Michelson interferometer)</h1>
       <div>
-        <label for="lambda">Lambda:</label>
-        <input type="range" id="lambda" name="lambda" min="0" max="10" step="1" value={Lambda} onChange={handleSliderChangeL}></input>
+        <label htmlFor="lambda">Lambda:</label>
+        <input type="range" id="lambda" name="lambda" min="0" max="10" step="1" value={Lambda} onChange={handleSliderChangeL} />
         {Lambda}
-        
+
       </div>
       <div>
-        <label for="s">s:</label>
-        <input type="range" id="s" name="s" min="10" max="30" step="1" value={S} onChange={handleSliderChangeS}></input>
+        <label htmlFor="s">s:</label>
+        <input type="range" id="s" name="s" min="10" max="30" step="1" value={S} onChange={handleSliderChangeS} />
         {S}
       </div>
       <div>
-        <label for="a">a:</label>
-        <input type="range" id="a" name="a" min="1" max="5" step="1" value={A} onChange={handleSliderChangeA}></input>
+        <label htmlFor="a">a:</label>
+        <input type="range" id="a" name="a" min="1" max="5" step="1" value={A} onChange={handleSliderChangeA} />
         {A}
       </div>
     </div>
