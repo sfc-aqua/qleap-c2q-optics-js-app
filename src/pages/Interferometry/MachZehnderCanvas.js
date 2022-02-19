5ade17f33e7eafd09678b578704cd8f34628bb1e
Yasuhiro Ohkura <rum.yasuhiro@gmail.com>
2022-02-19 22:49:48 +0900

    rotate draw

--- src/pages/Interferometry/MachZehnderCanvas.js
+++ src/pages/Interferometry/MachZehnderCanvas.js
@@ -1,17 +1,9 @@
 import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import useAnimationFrame from "../../useAnimation";
+import SingleRect from "./singleRect";
 import DoubleRect from "./doubleRect";
 
-const source = new DoubleRect(50, 450, 50, 50, 0, 0, 1);
-
-const splitter1 = new DoubleRect(50, 350, 50, 5, 135, 2, 1);
-const splitter2 = new DoubleRect(250, 150, 50, 5, 135, 2, 1);
-
-const mirror1 = new DoubleRect(50, 150, 50, 5, 135, 1, 2);
-const mirror2 = new DoubleRect(250, 350, 50, 5, -45, 1, 2);
-
-const sample = new DoubleRect(50, 250, 50, -30, 0, 1, 0);
 
 function MachZehnderCanvas({ size: { width, height }, refractionIndex }) {
   const cvs = useRef(null);
@@ -21,13 +13,47 @@ function MachZehnderCanvas({ size: { width, height }, refractionIndex }) {
     const ctx = cvs.current.getContext("2d");
     ctx.clearRect(0, 0, width, height);
 
+    const source = new SingleRect(0.1 * width, 0.75 * height, 50, 50, 45);
+
+    const bs1 = new DoubleRect(0.3 * width, 0.5 * height, 100, 5, 180, 2, 1);
+    const bs2 = new DoubleRect(0.7 * width, 0.5 * height, 100, 5, 0, 2, 1);
+
+    const mirror1 = new DoubleRect(0.5 * width, 0.25 * height, 100, 5, 180, 1, 2);
+    const mirror2 = new DoubleRect(0.5 * width, 0.75 * height, 100, 5, 0, 1, 2);
+
+    const detector0 = new SingleRect(0.9 * width, 0.25 * height, 50, 50, -135);
+    const detector1 = new SingleRect(0.9 * width, 0.75 * height, 50, 50, -45);
+
+    const sample = new DoubleRect(50, 250, 50, -30, 0, 1, 0);
+
     // show objects
     source.draw(ctx);
-    splitter1.draw(ctx);
-    splitter2.draw(ctx);
-
+    bs1.draw(ctx);
+    bs2.draw(ctx);
     mirror1.draw(ctx);
     mirror2.draw(ctx);
+    detector0.draw(ctx);
+    detector1.draw(ctx);
+
+    // show path of photon
+    const drawThePhotonPath = (originObj, endObj) => {
+      ctx.beginPath();
+      ctx.lineWidth = 5;
+      ctx.setLineDash([10, 5]);
+      ctx.moveTo(originObj.x, originObj.y);
+      ctx.lineTo(endObj.x, endObj.y);
+      ctx.stroke();
+      ctx.lineWidth = 1;
+      ctx.setLineDash([]);
+    };
+
+    drawThePhotonPath(source, bs1);
+    drawThePhotonPath(bs1, mirror1);
+    drawThePhotonPath(bs1, mirror2);
+    drawThePhotonPath(mirror1, bs2);
+    drawThePhotonPath(mirror2, bs2);
+    drawThePhotonPath(bs2, detector0);
+    drawThePhotonPath(bs2, detector1);
 
     // show wave propagation
     // wave properties
@@ -35,77 +61,77 @@ function MachZehnderCanvas({ size: { width, height }, refractionIndex }) {
     const waveNumberK = 0.2;
     const lightBeamWidth = 2;
 
-    // from light source to splitter 1
-    for (let posY = splitter1.y; posY < source.y; posY++) {
-      ctx.fillRect(
-        amplitude * Math.sin(waveNumberK * posY) + source.x,
-        posY,
-        lightBeamWidth,
-        lightBeamWidth,
-      );
-    }
+    // // from light source to splitter 1
+    // for (let posY = splitter1.y; posY < source.y; posY++) {
+    //   ctx.fillRect(
+    //     amplitude * Math.sin(waveNumberK * posY) + source.x,
+    //     posY,
+    //     lightBeamWidth,
+    //     lightBeamWidth,
+    //   );
+    // }
 
-    // x-axis waves
-    for (let posX = mirror1.x; posX < mirror2.x; posX++) {
-      ctx.fillRect(
-        posX,
-        amplitude * Math.sin(waveNumberK * posX) + splitter1.y,
-        lightBeamWidth,
-        lightBeamWidth,
-      );
-      ctx.fillRect(
-        posX,
-        amplitude * Math.sin(waveNumberK * posX) + splitter2.y,
-        lightBeamWidth,
-        lightBeamWidth,
-      );
-    }
-    // x-axis: splitter to detector
-    for (let posX = splitter2.x; posX < 350; posX++) {
-      ctx.fillRect(
-        posX,
-        amplitude * Math.sin(waveNumberK * posX) + splitter2.y,
-        lightBeamWidth,
-        lightBeamWidth,
-      );
-    }
-    // y-axis waves
-    for (let posY = splitter2.y; posY < splitter1.y; posY++) {
-      ctx.fillRect(
-        amplitude * Math.sin(waveNumberK * posY) + splitter1.x,
-        posY,
-        lightBeamWidth,
-        lightBeamWidth,
-      );
-      ctx.fillRect(
-        amplitude * Math.sin(waveNumberK * posY) + splitter2.x,
-        posY,
-        lightBeamWidth,
-        lightBeamWidth,
-      );
-    }
-    // y-axis waves: splitter to detector
-    for (let posY = 50; posY < splitter2.y; posY++) {
-      ctx.fillRect(
-        amplitude * Math.sin(waveNumberK * posY) + splitter2.x,
-        posY,
-        lightBeamWidth,
-        lightBeamWidth,
-      );
-    }
+    // // x-axis waves
+    // for (let posX = mirror1.x; posX < mirror2.x; posX++) {
+    //   ctx.fillRect(
+    //     posX,
+    //     amplitude * Math.sin(waveNumberK * posX) + splitter1.y,
+    //     lightBeamWidth,
+    //     lightBeamWidth,
+    //   );
+    //   ctx.fillRect(
+    //     posX,
+    //     amplitude * Math.sin(waveNumberK * posX) + splitter2.y,
+    //     lightBeamWidth,
+    //     lightBeamWidth,
+    //   );
+    // }
+    // // x-axis: splitter to detector
+    // for (let posX = splitter2.x; posX < 350; posX++) {
+    //   ctx.fillRect(
+    //     posX,
+    //     amplitude * Math.sin(waveNumberK * posX) + splitter2.y,
+    //     lightBeamWidth,
+    //     lightBeamWidth,
+    //   );
+    // }
+    // // y-axis waves
+    // for (let posY = splitter2.y; posY < splitter1.y; posY++) {
+    //   ctx.fillRect(
+    //     amplitude * Math.sin(waveNumberK * posY) + splitter1.x,
+    //     posY,
+    //     lightBeamWidth,
+    //     lightBeamWidth,
+    //   );
+    //   ctx.fillRect(
+    //     amplitude * Math.sin(waveNumberK * posY) + splitter2.x,
+    //     posY,
+    //     lightBeamWidth,
+    //     lightBeamWidth,
+    //   );
+    // }
+    // // y-axis waves: splitter to detector
+    // for (let posY = 50; posY < splitter2.y; posY++) {
+    //   ctx.fillRect(
+    //     amplitude * Math.sin(waveNumberK * posY) + splitter2.x,
+    //     posY,
+    //     lightBeamWidth,
+    //     lightBeamWidth,
+    //   );
+    // }
 
-    // sample which changes speed of light inside corresponding to the refractive index
-    ctx.clearRect(sample.x - sample.w / 2, sample.y, sample.w, -sample.h);
-    sample.draw(ctx);
-    // y-axis waves: inside sample
-    for (let posY = sample.y; posY < sample.y - sample.h; posY++) {
-      ctx.fillRect(
-        amplitude * Math.sin(waveNumberK * posY * refractionIndex) + sample.x,
-        posY,
-        lightBeamWidth,
-        lightBeamWidth,
-      );
-    }
+  //   // sample which changes speed of light inside corresponding to the refractive index
+  //   ctx.clearRect(sample.x - sample.w / 2, sample.y, sample.w, -sample.h);
+  //   sample.draw(ctx);
+  //   // y-axis waves: inside sample
+  //   for (let posY = sample.y; posY < sample.y - sample.h; posY++) {
+  //     ctx.fillRect(
+  //       amplitude * Math.sin(waveNumberK * posY * refractionIndex) + sample.x,
+  //       posY,
+  //       lightBeamWidth,
+  //       lightBeamWidth,
+  //     );
+  //   }
   });
   return (
     <canvas
---
+++ src/pages/Interferometry/singleRect.js
@@ -0,0 +1,22 @@
+class SingleRect {
+    constructor(x, y, w = 50, h = 50, angle = 0) {
+      this.x = x;
+      this.y = y;
+      this.w = w;
+      this.h = h;
+      this.angle = angle;
+      this.radian = this.angle * (Math.PI / 180);
+    }
+  
+    draw(ctx) {
+      ctx.save();
+      ctx.translate(this.x, this.y);
+      ctx.rotate(this.radian);
+      // adjust position as a center top of filled rectangle
+      ctx.fillRect(-0.5 * this.w, 0, this.w, this.h);
+      ctx.restore();
+    }
+  }
+  
+  export default SingleRect;
+  
