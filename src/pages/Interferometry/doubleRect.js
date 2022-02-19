60405832f4cf715f96dc296d03543c6e9b37d1a8
Yasuhiro Ohkura <rum.yasuhiro@gmail.com>
2022-02-20 00:59:47 +0900

    put objects outside of function for eneble to refer from others

--- src/pages/Interferometry/doubleRect.js
+++ src/pages/Interferometry/doubleRect.js
@@ -1,5 +1,17 @@
 class DoubleRect {
-  constructor(x, y, w = 50, h = 50, angle = 0, strokeRatio = 0, fillRatio = 1, name) {
+  constructor(name) {
+    this.x = 0;
+    this.y = 0;
+    this.w = 0;
+    this.h = 0;
+    this.angle = 0;
+    this.radian = 0;
+    this.strokeRatio = 0;
+    this.fillRatio = 0;
+    this.name = name;
+  }
+
+  draw(context, x, y, w = 50, h = 50, angle = 0, strokeRatio = 0, fillRatio = 1) {
     this.x = x;
     this.y = y;
     this.w = w;
@@ -8,10 +20,7 @@ class DoubleRect {
     this.radian = this.angle * (Math.PI / 180);
     this.strokeRatio = strokeRatio;
     this.fillRatio = fillRatio;
-    this.name = name;
-  }
 
-  draw(context) {
     context.save();
     context.translate(this.x, this.y);
     context.rotate(this.radian);
--- src/pages/Interferometry/singleRect.js
+++ src/pages/Interferometry/singleRect.js
@@ -1,15 +1,21 @@
 class SingleRect {
-  constructor(x, y, w = 50, h = 50, angle = 0, name = null) {
+  constructor(name = null) {
+    this.x = 0;
+    this.y = 0;
+    this.w = 0;
+    this.h = 0;
+    this.angle = 0;
+    this.radian = 0;
+    this.name = name;
+  }
+
+  draw(context, x, y, w = 50, h = 50, angle = 0) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.angle = angle;
     this.radian = this.angle * (Math.PI / 180);
-    this.name = name;
-  }
-
-  draw(context) {
     context.save();
     context.translate(this.x, this.y);
     context.rotate(this.radian);
