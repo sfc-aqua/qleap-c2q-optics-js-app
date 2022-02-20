b59de7d14ff33076cf79f7ad87b013353a65f8bd
Yasuhiro Ohkura <rum.yasuhiro@gmail.com>
2022-02-20 18:20:26 +0900

    divide functionarity as a class and new file  to simplify the MachZehnderCanvas

--- src/pages/Interferometry/MachZehnderCanvas.js
+++ src/pages/Interferometry/MachZehnderCanvas.js
@@ -1,59 +1,7 @@
 import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import useAnimationFrame from "../../useAnimation";
-import SingleRect from "./singleRect";
-import DoubleRect from "./doubleRect";
-
-// show path of photon
-const drawThePhotonPath = (context, originObj, destinationObj) => {
-  context.beginPath();
-  context.lineWidth = 5;
-  context.setLineDash([10, 5]);
-  context.moveTo(originObj.x, originObj.y);
-  context.lineTo(destinationObj.x, destinationObj.y);
-  context.stroke();
-  context.lineWidth = 1;
-  context.setLineDash([]);
-};
-
-const drawMachZehnderEquipments = (
-  context,
-  x,
-  y,
-  width,
-  height,
-  source,
-  bs1,
-  bs2,
-  mirror0,
-  mirror1,
-  detector0,
-  detector1,
-) => {
-  context.save();
-  context.translate(x, y);
-
-  // const sample = new DoubleRect(50, 250, 50, -30, 0, 1, 0);
-
-  // show objects
-  source.draw(context, 0, 0.75 * height, 50, 50, 45);
-  bs1.draw(context, 0.25 * width, 0.5 * height, 100, 5, 180, 2, 1);
-  bs2.draw(context, 0.75 * width, 0.5 * height, 100, 5, 0, 2, 1);
-  mirror0.draw(context, 0.5 * width, 0.25 * height, 100, 5, 180, 1, 2);
-  mirror1.draw(context, 0.5 * width, 0.75 * height, 100, 5, 0, 1, 2);
-  detector0.draw(context, width, 0.25 * height, 50, 50, -135);
-  detector1.draw(context, width, 0.75 * height, 50, 50, -45);
-
-  drawThePhotonPath(context, source, bs1);
-  drawThePhotonPath(context, bs1, mirror0);
-  drawThePhotonPath(context, bs1, mirror1);
-  drawThePhotonPath(context, mirror0, bs2);
-  drawThePhotonPath(context, mirror1, bs2);
-  drawThePhotonPath(context, bs2, detector0);
-  drawThePhotonPath(context, bs2, detector1);
-
-  context.restore();
-};
+import MachZehnderEquipment from "./MachZehnderEquipment";
 
 function MachZehnderCanvas({ size: { width, height } }) {
   const cvs = useRef(null);
@@ -63,28 +11,14 @@ function MachZehnderCanvas({ size: { width, height } }) {
     const ctx = cvs.current.getContext("2d");
     ctx.clearRect(0, 0, width, height);
 
-    const source = new SingleRect("Photon Source");
-    const bs1 = new DoubleRect("BS1");
-    const bs2 = new DoubleRect("BS2");
-    const mirror0 = new DoubleRect("mirror");
-    const mirror1 = new DoubleRect("mirror");
-    const detector0 = new SingleRect("D0");
-    const detector1 = new SingleRect("D1");
-
-    drawMachZehnderEquipments(
+    const equipment = new MachZehnderEquipment(
       ctx,
       100,
       100,
       400,
       400,
-      source,
-      bs1,
-      bs2,
-      mirror0,
-      mirror1,
-      detector0,
-      detector1,
     );
+    equipment.draw();
   });
   return (
     <canvas
---
+++ src/pages/Interferometry/MachZehnderEquipment.js
@@ -0,0 +1,59 @@
+import SingleRect from "./singleRect";
+import DoubleRect from "./doubleRect";
+
+// show path of photon
+const drawThePhotonPath = (context, originObj, destinationObj, lineWidth = 1) => {
+  context.beginPath();
+  context.lineWidth = 5;
+  context.setLineDash([10, 5]);
+  context.moveTo(originObj.x, originObj.y);
+  context.lineTo(destinationObj.x, destinationObj.y);
+  context.stroke();
+  context.lineWidth = lineWidth;
+  context.setLineDash([]);
+};
+
+class MachZehnderEquipment {
+  constructor(context, posX, posY, width, height) {
+    this.context = context;
+    this.posX = posX;
+    this.posY = posY;
+    this.width = width;
+    this.height = height;
+
+    this.source = new SingleRect("Photon Source");
+    this.bs1 = new DoubleRect("BS1");
+    this.bs2 = new DoubleRect("BS2");
+    this.mirror0 = new DoubleRect("mirror");
+    this.mirror1 = new DoubleRect("mirror");
+    this.detector0 = new SingleRect("D0");
+    this.detector1 = new SingleRect("D1");
+  }
+
+  draw() {
+    this.context.save();
+    this.context.translate(this.posX, this.posY);
+    // const sample = new DoubleRect(50, 250, 50, -30, 0, 1, 0);
+
+    // show objects
+    this.source.draw(this.context, 0, 0.75 * this.height, 50, 50, 45);
+    this.bs1.draw(this.context, 0.25 * this.width, 0.5 * this.height, 100, 5, 180, 2, 1);
+    this.bs2.draw(this.context, 0.75 * this.width, 0.5 * this.height, 100, 5, 0, 2, 1);
+    this.mirror0.draw(this.context, 0.5 * this.width, 0.25 * this.height, 100, 5, 180, 1, 2);
+    this.mirror1.draw(this.context, 0.5 * this.width, 0.75 * this.height, 100, 5, 0, 1, 2);
+    this.detector0.draw(this.context, this.width, 0.25 * this.height, 50, 50, -135);
+    this.detector1.draw(this.context, this.width, 0.75 * this.height, 50, 50, -45);
+
+    drawThePhotonPath(this.context, this.source, this.bs1);
+    drawThePhotonPath(this.context, this.bs1, this.mirror0);
+    drawThePhotonPath(this.context, this.bs1, this.mirror1);
+    drawThePhotonPath(this.context, this.mirror0, this.bs2);
+    drawThePhotonPath(this.context, this.mirror1, this.bs2);
+    drawThePhotonPath(this.context, this.bs2, this.detector0);
+    drawThePhotonPath(this.context, this.bs2, this.detector1);
+
+    this.context.restore();
+  }
+}
+
+export default MachZehnderEquipment;
