311adc7425cd4f719f47bbbffc248f73c7e30837
Yasuhiro Ohkura <rum.yasuhiro@gmail.com>
2022-03-22 18:05:12 +0900

    fix term from x to posX and y to posY

--- src/pages/Interferometry/singleRect.js
+++ src/pages/Interferometry/singleRect.js
@@ -1,23 +1,17 @@
 class SingleRect {
-  constructor(name = null) {
-    this.x = 0;
-    this.y = 0;
-    this.w = 0;
-    this.h = 0;
-    this.angle = 0;
-    this.radian = 0;
-    this.name = name;
-  }
-
-  draw(context, x, y, w = 50, h = 50, angle = 0) {
-    this.x = x;
-    this.y = y;
+  constructor(name, x, y, w = 50, h = 50, angle = 0) {
+    this.posX = x;
+    this.posY = y;
     this.w = w;
     this.h = h;
     this.angle = angle;
     this.radian = this.angle * (Math.PI / 180);
+    this.name = name;
+  }
+
+  draw(context) {
     context.save();
-    context.translate(this.x, this.y);
+    context.translate(this.posX, this.posY);
     context.rotate(this.radian);
     // adjust position as a center top of filled rectangle
     context.fillRect(-0.5 * this.w, 0, this.w, this.h);
