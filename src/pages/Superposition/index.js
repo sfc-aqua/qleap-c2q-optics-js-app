ffe443bd5785d7dc65f23a8309c57d49cde72cd9
zigen <hrlclb@gmail.com>
2022-03-28 15:44:24 +0900

    add MODE button

--- src/pages/Superposition/index.js
+++ src/pages/Superposition/index.js
@@ -14,7 +14,7 @@ const useWaves = (numWaves) => {
         k: 0.01,
         omega: 1,
         amplitude: i * 0.2 + 0.5,
-        phase: 0.1,
+        phase: 0,
         visible: [0, 1].includes(i),
         color: `hsl(${(i * 360) / numWaves}, 100%, 50%)`,
         selected: false,
@@ -30,49 +30,62 @@ const useWaves = (numWaves) => {
   return [waves, setter];
 };
 
+const MODE = {
+  SUPERPOSITION: "SUPERPOSITION",
+  DECONSTRUCTION: "DECONSTRUCTION",
+};
+
 function Superposition() {
   const [waves, setWave] = useWaves(NUM_WAVES);
   const [playing, setPlaying] = useState(true);
+  const [mode, setMode] = useState(MODE.SUPERPOSITION);
 
-  const drawWave = useCallback((ctx, timestamp) => {
-    const t = playing ? timestamp : 0;
-    const { width } = ctx.canvas;
-    const composites = Array(width).fill(0);
-    for (let i = 0; i < NUM_WAVES; i++) {
-      const {
-        k, amplitude, phase, omega, visible, color, selected,
-      } = waves[i];
+  const drawSuperpositionWave = useCallback(
+    (ctx, timestamp) => {
+      const t = playing ? timestamp : 0;
+      const { width } = ctx.canvas;
+      const composites = Array(width).fill(0);
+      for (let i = 0; i < NUM_WAVES; i++) {
+        const {
+          k, amplitude, phase, omega, visible, color, selected,
+        } = waves[i];
 
-      ctx.beginPath();
-      ctx.lineWidth = selected ? 4 : 1;
-      for (let x = 0; x < width; x++) {
-        if (visible) {
-          ctx.strokeStyle = color;
-          const y = Math.cos(k * x - omega * t * 0.005 + phase) * amplitude * 90;
-          ctx.lineTo(x, y);
-          composites[x] += y;
+        ctx.beginPath();
+        ctx.lineWidth = selected ? 4 : 1;
+        for (let x = 0; x < width; x++) {
+          if (visible) {
+            ctx.strokeStyle = color;
+            const y = Math.cos(k * x - omega * t * 0.005 + phase) * amplitude * 90;
+            ctx.lineTo(x, y);
+            composites[x] += y;
+          }
         }
+        ctx.stroke();
+      }
+
+      ctx.strokeStyle = "black";
+      ctx.beginPath();
+
+      for (let x = 0; x < width; x++) {
+        ctx.lineTo(x, composites[x]);
       }
       ctx.stroke();
-    }
-
-    ctx.strokeStyle = "black";
-    ctx.beginPath();
-
-    for (let x = 0; x < width; x++) {
-      ctx.lineTo(x, composites[x]);
-    }
-    ctx.stroke();
-  }, [playing, waves]);
+    },
+    [playing, waves],
+  );
 
   return (
     <div>
       <h1>Superposition of Traveling Waves</h1>
-      <WaveCanvas drawFunc={drawWave} />
+      <WaveCanvas drawFunc={drawSuperpositionWave} />
       <div>
         <button type="button" onClick={() => setPlaying(!playing)}>
           {playing ? "pause" : "play"}
         </button>
+        <select onChange={(e) => setMode(e.target.value)} defaultValue={mode}>
+          <option value={MODE.SUPERPOSITION}>Superposition</option>
+          <option value={MODE.DECONSTRUCTION}>Deconstruction</option>
+        </select>
       </div>
       <div className="waves-wrapper">
         {Object.keys(waves).map((i) => (
